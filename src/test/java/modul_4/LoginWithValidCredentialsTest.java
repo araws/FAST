aa61c4e7f824ce1cdd11fac0d2958ce72da3c676
Andrzej Rawski <rawski.andrzej@gmail.com>
2026-04-04 20:04:09 +0200

    refactored locators into recommended

--- src/test/java/modul_4/LoginWithValidCredentialsTest.java
+++ src/test/java/modul_4/LoginWithValidCredentialsTest.java
@@ -1,6 +1,8 @@
 package modul_4;
 
+import com.microsoft.playwright.Page;
 import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
 import common.BaseTest;
 import org.junit.jupiter.api.Test;
 
@@ -9,14 +11,14 @@ class LoginWithValidCredentialsTest extends BaseTest {
     @Test
     void should_login_with_valid_credentials() {
         page.navigate("https://the-internet.herokuapp.com/login");
-        page.fill("#username", "tomsmith");
-        page.fill("#password", "SuperSecretPassword!");
-        page.click("#login button");
+        page.getByLabel("Username").fill("tomsmith");
+        page.getByLabel("Password").fill("SuperSecretPassword!");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Login")).click();
 
-        PlaywrightAssertions.assertThat(page.locator("text=You logged into a secure area!")).isVisible();
+        PlaywrightAssertions.assertThat(page.getByText("You logged into a secure area!")).isVisible();
 
-        page.click("//i[contains(text(), 'Logout')]");
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Logout")).click();
 
-        PlaywrightAssertions.assertThat(page.locator("text=You logged out of the secure area!")).isVisible();
+        PlaywrightAssertions.assertThat(page.getByText("You logged out of the secure area!")).isVisible();
     }
 }
