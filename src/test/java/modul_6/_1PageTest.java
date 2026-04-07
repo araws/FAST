013f404ed4e7b227ec8c688e7157da8cf56a72c2
Andrzej Rawski <rawski.andrzej@gmail.com>
2026-04-07 09:45:32 +0200

    text methods

--- src/test/java/modul_6/_1PageTest.java
+++ src/test/java/modul_6/_1PageTest.java
@@ -1,4 +1,4 @@
-package madul_6;
+package modul_6;
 
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.KeyboardModifier;
---
+++ src/test/java/modul_6/_3TextTest.java
@@ -0,0 +1,24 @@
+package modul_6;
+
+import common.BaseTest;
+import org.junit.jupiter.api.Test;
+
+public class _3TextTest extends BaseTest {
+
+    @Test
+    void text_test(){
+        page.navigate("https://the-internet.herokuapp.com/notification_message_rendered");
+
+//        1. .innerHTML() - tags + text
+//        System.out.println(page.innerHTML("#content"));
+
+//        2. .innerText() - only text (visible on page)
+//        System.out.println(page.innerText("#content"));
+
+//        3. .textContent() - only text (visible and hidden on page)
+//        System.out.println(page.textContent("#content"));
+
+//        4. .getAttribute() - HTML attributes
+//        System.out.println(page.getAttribute("div img", "alt"));
+    }
+}
