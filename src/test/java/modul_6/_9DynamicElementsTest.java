43e80226e8e9fd8e274563cf12e9a96ae301b5e9
Andrzej Rawski <rawski.andrzej@gmail.com>
2026-04-22 19:23:41 +0200

    dynamic controls tests created

--- src/test/java/modul_6/_9DynamicElementsTest.java
+++ src/test/java/modul_6/_9DynamicElementsTest.java
@@ -2,23 +2,24 @@ package modul_6;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import com.microsoft.playwright.options.AriaRole;
 import common.BaseTest;
 import org.junit.jupiter.api.Test;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 public class _9DynamicElementsTest extends BaseTest {
 
     @Test
-    void dynamicElementTest() {
+    void dynamicElementTest1() {
         page.navigate("https://the-internet.herokuapp.com/dynamic_loading/1");
         Locator text = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Hello World!"));
 
-        PlaywrightAssertions.assertThat(text).not().isVisible();
+        assertThat(text).not().isVisible();
 
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Start")).click();
 
-        PlaywrightAssertions.assertThat(text).isVisible();
+        assertThat(text).isVisible();
     }
 
     @Test
@@ -26,10 +27,36 @@ public class _9DynamicElementsTest extends BaseTest {
         page.navigate("https://the-internet.herokuapp.com/dynamic_loading/2");
         Locator text = page.getByRole(AriaRole.HEADING, new Page.GetByRoleOptions().setName("Hello World!"));
 
-        PlaywrightAssertions.assertThat(text).not().isVisible();
+        assertThat(text).not().isVisible();
 
         page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Start")).click();
 
-        PlaywrightAssertions.assertThat(text).isVisible();
+        assertThat(text).isVisible();
+    }
+
+    @Test
+    void dynamicControlsTest1() {
+        page.navigate("https://the-internet.herokuapp.com/dynamic_controls");
+        Locator checkbox = page.locator("#checkbox");
+
+        assertThat(checkbox).isVisible();
+
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Remove")).click();
+
+        assertThat(checkbox).not().isVisible();
+        assertThat(page.getByText("It's gone!")).isVisible();
+    }
+
+    @Test
+    void dynamicControlsTest2() {
+        page.navigate("https://the-internet.herokuapp.com/dynamic_controls");
+        Locator input = page.locator("#input-example input[type=text]");
+
+        assertThat(input).isDisabled();
+
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Enable")).click();
+
+        assertThat(input).isEnabled();
+        assertThat(page.getByText("It's enabled!")).isVisible();
     }
 }
