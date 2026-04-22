8d1e0ad2be67500f378b23b8ed4904c6fac28de0
Andrzej Rawski <rawski.andrzej@gmail.com>
2026-04-22 13:20:38 +0200

    Assertions added

--- src/test/java/modul_6/_5CheckboxTest.java
+++ src/test/java/modul_6/_5CheckboxTest.java
@@ -1,5 +1,7 @@
 package modul_6;
 
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import com.microsoft.playwright.options.AriaRole;
 import common.BaseTest;
 import org.junit.jupiter.api.Test;
@@ -7,10 +9,22 @@ import org.junit.jupiter.api.Test;
 public class _5CheckboxTest extends BaseTest {
 
     @Test
-    void checkboxTest(){
+    void checkboxTest() {
         page.navigate("https://the-internet.herokuapp.com/checkboxes");
-        page.getByRole(AriaRole.CHECKBOX).first().check();
-        page.getByRole(AriaRole.CHECKBOX).last().uncheck();
 
+        Locator firstCheckbox = page.getByRole(AriaRole.CHECKBOX).first();
+        Locator lastCheckbox = page.getByRole(AriaRole.CHECKBOX).last();
+
+        PlaywrightAssertions.assertThat(firstCheckbox).not().isChecked();
+
+        firstCheckbox.check();
+
+        PlaywrightAssertions.assertThat(firstCheckbox).isChecked();
+
+        PlaywrightAssertions.assertThat(lastCheckbox).isChecked();
+
+        lastCheckbox.uncheck();
+
+        PlaywrightAssertions.assertThat(lastCheckbox).not().isChecked();
     }
 }
