6b6e459ee5a22f77b084c1f61a2afe05f34e9405
Andrzej Rawski <rawski.andrzej@gmail.com>
2026-04-26 10:50:26 +0200

    default context restored

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -24,13 +24,13 @@ public class BaseTest {
 
     @BeforeEach
     void beforeEach() {
-//        context = browser.newContext();
+        context = browser.newContext();
 
         // VIDEO RECORDING FROM TEST
-        context = browser.newContext(new Browser.NewContextOptions()
-                .setViewportSize(1920, 1080)
-                .setRecordVideoDir(Paths.get("videos/"))
-                .setRecordVideoSize(new RecordVideoSize(1920, 1080)));
+//        context = browser.newContext(new Browser.NewContextOptions()
+//                .setViewportSize(1920, 1080)
+//                .setRecordVideoDir(Paths.get("videos/"))
+//                .setRecordVideoSize(new RecordVideoSize(1920, 1080)));
 
         page = context.newPage();
     }
